e3bf1eeb645fff7a34598f0f3847e08c89e1941d
jacobraim <jacob.raim@gmail.com>
2026-07-28 11:58:34 -0400

    Update puzzles.js

--- puzzles.js
+++ puzzles.js
@@ -7,52 +7,53 @@
  */
 
 const DC_LINK_PUZZLES = {
-'2026-07-28': {
-  number: 1,
-  groups: [
-    {
-      id: 'green',
-      label: 'Places beginning with “Union”',
-      color: 'green',
-      items: [
-        'Union Station',
-        'Union Market',
-        'Union Stage',
-        'Union Pub'
-      ]
-    },
-    {
-      id: 'yellow',
-      label: 'DC traffic circles',
-      color: 'yellow',
-      items: [
-        'Dupont Circle',
-        'Logan Circle',
-        'Thomas Circle',
-        'Scott Circle'
-      ]
-    },
-    {
-      id: 'blue',
-      label: 'DC music venues',
-      color: 'blue',
-      items: [
-        '9:30 Club',
-        'Black Cat',
-        'The Anthem',
-        'Howard Theatre'
-      ]
-    },
-    {
-      id: 'purple',
-      label: 'Green Line Metro stations',
-      color: 'purple',
-      items: [
-        'Navy Yard-Ballpark',
-        'Shaw-Howard U',
-        'U Street',
-        'Waterfront'
-      ]
-    }
-  ]
+  '2026-07-28': {
+    number: 1,
+    groups: [
+      {
+        id: 'green',
+        label: 'Places beginning with “Union”',
+        color: 'green',
+        items: [
+          'Union Station',
+          'Union Market',
+          'Union Stage',
+          'Union Pub'
+        ]
+      },
+      {
+        id: 'yellow',
+        label: 'DC traffic circles',
+        color: 'yellow',
+        items: [
+          'Dupont Circle',
+          'Logan Circle',
+          'Thomas Circle',
+          'Scott Circle'
+        ]
+      },
+      {
+        id: 'blue',
+        label: 'DC music venues',
+        color: 'blue',
+        items: [
+          '9:30 Club',
+          'Black Cat',
+          'The Anthem',
+          'Howard Theatre'
+        ]
+      },
+      {
+        id: 'purple',
+        label: 'Green Line Metro stations',
+        color: 'purple',
+        items: [
+          'Navy Yard-Ballpark',
+          'Shaw-Howard U',
+          'U Street',
+          'Waterfront'
+        ]
+      }
+    ]
+  }
 };
